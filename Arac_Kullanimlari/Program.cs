57cec0303f8b238ba3128d9adeab7efff8938035
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-09-21 12:48:21 +0300

    feat: created new form

--- Arac_Kullanimlari/Program.cs
+++ Arac_Kullanimlari/Program.cs
@@ -16,7 +16,7 @@ namespace Arac_Kullanimlari
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
