52979f01b9a0937c2f79c943756b33fea9278a83
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-09-23 12:42:34 +0300

    feat: combobox process

--- Arac_Kullanimlari/Form2.Designer.cs
+++ Arac_Kullanimlari/Form2.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -49,12 +50,23 @@
             this.comboBox1.Size = new System.Drawing.Size(192, 32);
             this.comboBox1.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Şehir Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(693, 436);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
@@ -68,5 +80,6 @@
         #endregion
 
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Arac_Kullanimlari/Form2.cs
+++ Arac_Kullanimlari/Form2.cs
@@ -21,5 +21,10 @@ namespace Arac_Kullanimlari
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Antalya");
+        }
     }
 }
